4e53729e4cd9c45d1c938d28efa90c5fb5755f75
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-10-21 00:18:04 +0100

    docs(utility): Remove `.` on `@param`

--- source/utility.ts
+++ source/utility.ts
@@ -1,7 +1,7 @@
 /**
  * Transforms a name into a variant that is able to be safely sent to the API.
  *
- * @param name - The name to transform.
+ * @param name - The name to transform
  * @returns The transformed name.
  * @internal
  */
