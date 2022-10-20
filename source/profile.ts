a28edd8d3ff8f30a616664cd80763007fa484d40
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-10-20 19:51:51 +0100

    feat: Add RuneMetrics profile

---
+++ source/profile.ts
@@ -0,0 +1,296 @@
+import { URLSearchParams } from "node:url";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import { request } from "undici";
+import { transformName } from "./utility.js";
+
+interface RawProfile {
+	magic: number;
+	questsstarted: number;
+	totalskill: number;
+	questscomplete: number;
+	questsnotstarted: number;
+	totalxp: number;
+	ranged: number;
+	activities: RawProfileActivity[];
+	skillvalues: ProfileSkills[];
+	name: string;
+	rank: string | null;
+	melee: number;
+	combatlevel: number;
+	loggedIn: "true" | "false";
+}
+
+/**
+ * Represents the type of error that may be encountered when fetching a player's RuneMetrics profile.
+ *
+ * @internal
+ */
+const enum ProfileErrorType {
+	Private = "PROFILE_PRIVATE",
+	Unknown = "NO_PROFILE",
+}
+
+/**
+ * Represents an error returned when fetching a player's RuneMetrics profile.
+ *
+ * @internal
+ */
+interface ProfileError<T extends ProfileErrorType = ProfileErrorType> {
+	error: T;
+	loggedIn: "false";
+}
+
+/**
+ * Represents the returned RuneMetrics profile activity.
+ *
+ * @internal
+ */
+interface RawProfileActivity {
+	date: string;
+	details: string;
+	text: string;
+}
+
+/**
+ * Represents the RuneMetrics profile activity.
+ */
+export interface ProfileActivity {
+	/**
+	 * The timestamp of the activity.
+	 */
+	timestamp: number;
+	/**
+	 * The details of the activity.
+	 *
+	 * @example "I killed 2 tormented demons, fewer tormented souls in the world must be a good thing."
+	 */
+	details: string;
+	/**
+	 * The shortened variant of the {@link details}.
+	 *
+	 * @example "I killed 2 tormented demons."
+	 */
+	text: string;
+}
+
+/**
+ * Represents the enumeration of skills as returned from the API.
+ */
+export enum Skill {
+	Attack,
+	Defence,
+	Strength,
+	Constitution,
+	Ranged,
+	Prayer,
+	Magic,
+	Cooking,
+	Woodcutting,
+	Fletching,
+	Fishing,
+	Firemaking,
+	Crafting,
+	Smithing,
+	Mining,
+	Herblore,
+	Agility,
+	Thieving,
+	Slayer,
+	Farming,
+	Runecrafting,
+	Hunter,
+	Construction,
+	Summoning,
+	Dungeoneering,
+	Divination,
+	Invention,
+	Archaeology,
+}
+
+/**
+ * Represents data about the player's stats as returned from the RuneMetrics profile.
+ */
+export interface ProfileSkills {
+	/**
+	 * The level of the skill.
+	 */
+	level: number;
+	/**
+	 * The experience of the skill.
+	 *
+	 * @remarks The experience is accurate to one decimal place.
+	 * @example 200000000.0
+	 */
+	xp: number;
+	/**
+	 * The rank this skill is in the HiScores.
+	 */
+	rank: number;
+	/**
+	 * The id of this skill.
+	 *
+	 * @remarks This is the specific number belonging to a skill. The {@link Skill} enumeration may help here.
+	 */
+	id: Skill;
+}
+
+/**
+ * Represents data about player's RuneMetrics Profile.
+ */
+export interface Profile {
+	/**
+	 * It's not known what this represents.
+	 *
+	 * @experimental
+	 */
+	magic: RawProfile["magic"];
+	/**
+	 * The amount of quests the player has started.
+	 */
+	questsStarted: RawProfile["questsstarted"];
+	/**
+	 * The total level of the player.
+	 */
+	totalSkill: RawProfile["totalskill"];
+	/**
+	 * The amount of quests the player has completed.
+	 */
+	questsComplete: RawProfile["questscomplete"];
+	/**
+	 * The amount of quests the player has not started.
+	 */
+	questsNotStarted: RawProfile["questsnotstarted"];
+	/**
+	 * The total experience of the player.
+	 */
+	totalXp: RawProfile["totalxp"];
+	/**
+	 * It's not known what this represents.
+	 *
+	 * @experimental
+	 */
+	ranged: RawProfile["ranged"];
+	/**
+	 * The activities of the player.
+	 *
+	 * @remarks This is the event log of the player; formerly known as the Adventurer's Log.
+	 */
+	activities: ProfileActivity[];
+	/**
+	 * An array of skill values of the player.
+	 *
+	 * @remarks This is an array of the player's skills containing various information.
+	 */
+	skillValues: RawProfile["skillvalues"];
+	/**
+	 * The name of the player.
+	 *
+	 * @remarks This returns the corrects the capitalisation of the player's name.
+	 */
+	name: RawProfile["name"];
+	/**
+	 * The rank of the player.
+	 *
+	 * @remarks When not `null`, this is separated by commas.
+	 * @example "8,181"
+	 */
+	rank: RawProfile["rank"];
+	/**
+	 * It's not known what this represents.
+	 *
+	 * @experimental
+	 */
+	melee: RawProfile["melee"];
+	/**
+	 * The combat level of the player.
+	 */
+	combatLevel: RawProfile["combatlevel"];
+	/**
+	 * Whether the player is signed in to RuneMetrics.
+	 *
+	 * @remarks Because of how this data is retrieved, this may be assumed to always be `false`.
+	 */
+	loggedIn: boolean;
+}
+
+/**
+ * Represents the options to provide for fetching the RuneMetrics profile.
+ */
+export interface ProfileOptions {
+	/**
+	 * The player's name.
+	 */
+	name: string;
+	/**
+	 * The maximum number of activities to return.
+	 *
+	 * @remarks 20 is the maximum number this limit abides by.
+	 */
+	activities?: number;
+}
+
+dayjs.extend(utc);
+
+/**
+ * Returns the player's profile data as viewed on RuneMetrics.
+ *
+ * @param options - The options to provide
+ * @returns The profile of the player as described by RuneMetrics.
+ */
+export async function profile({ name, activities }: ProfileOptions): Promise<Profile> {
+	const urlSearchParams = new URLSearchParams();
+	urlSearchParams.set("user", transformName(name));
+	if (typeof activities === "number") urlSearchParams.set("activities", String(activities));
+
+	const json = (await request(`https://apps.runescape.com/runemetrics/profile/profile?${urlSearchParams}`).then(
+		async ({ body }) => body.json(),
+	)) as RawProfile | ProfileError;
+
+	if ("error" in json) {
+		throw new Error(json.error);
+	}
+
+	const {
+		magic,
+		questsstarted,
+		totalskill,
+		questscomplete,
+		questsnotstarted,
+		totalxp,
+		ranged,
+		activities: rawActivities,
+		skillvalues,
+		name: rawName,
+		rank,
+		melee,
+		combatlevel,
+		loggedIn,
+	} = json;
+
+	return {
+		magic,
+		questsStarted: questsstarted,
+		totalSkill: totalskill,
+		questsComplete: questscomplete,
+		questsNotStarted: questsnotstarted,
+		totalXp: totalxp,
+		ranged,
+		activities: rawActivities.map(({ date, details, text }) => ({
+			timestamp: dayjs.utc(date, "Europe/London").valueOf(),
+			details,
+			text,
+		})),
+		skillValues: skillvalues.map(({ level, xp, rank, id }) => ({
+			level,
+			xp: xp / 10,
+			rank,
+			id,
+		})),
+		name: rawName,
+		rank,
+		melee,
+		combatLevel: combatlevel,
+		loggedIn: loggedIn === "true",
+	};
+}
